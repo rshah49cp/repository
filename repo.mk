6396e72b889cfd2afa6cdcbac6a5707a07ada90f
rshah49cp <rshah49@calpoly.edu>
2024-02-10 02:53:54 -0800

    update

--- repo.mk
+++ repo.mk
@@ -4,17 +4,21 @@
 # functions inspired from GNU Make manual:
 # https://www.gnu.org/software/make/manual/make.html#Rule-Introduction
 
-CC = gcc
-CFLAGS = -Wall -Wextra -Werror -pedantic -ansi
-ALL_C = IOstd.c safe_ops.c
-ALL_O = $(ALL_C:.c=.o)
+REPO_CC = gcc
+REPO_CFLAGS = -Wall -Wextra -Werror -pedantic -ansi
+REPO_C = IOstd.c safe_ops.c
+REPO_O = $(REPO_C:.c=.o)
+REPO_H = $(REPO_C:.c=.h) include_all.h
 
 .PHONY: all clean
 
-all: $(ALL_O)
+repo: $(REPO_O)
 
-%.o : %.c %.h include_all.h
-	$(CC) $(CFLAGS) -c $< -o $@
+IOstd.o: IOstd.c $(REPO_H) safe_ops.o
+	$(REPO_CC) $(REPO_CFLAGS) -c $< -o $@
+
+safe_ops.o: safe_ops.c safe_ops.h include_all.h
+	$(REPO_CC) $(REPO_CFLAGS) -c $< -o $@
 
 clean:
-	rm -f $(ALL_O)
+	rm -f *.o
